23f37ebcda579480b722feb70a9ec67bd96a726e
JoseRaul-TR <jraultr@gmail.com>
2025-03-15 21:08:05 +0100

    Added footer and implement responsivenes in navbar

--- scripts/navbar-script.js
+++ scripts/navbar-script.js
@@ -1,5 +1,18 @@
-/* Hamburger code */
+// Display current site in nav-list
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const currentPage = window.location.pathname.split('/').pop(); // Get filename from path
 
+    navLinks.forEach(link => {
+        const linkHref = link.getAttribute('href').split('/').pop(); // Get filename from href
+
+        if (linkHref === currentPage) {
+            link.classList.add('current'); // Add 'current' class for highlighting
+        }
+    });
+});
+
+// Toggle code
 const navToggle = document.querySelector('.nav-toggle');
 const mainNav = document.querySelector('.main-nav');
 
@@ -8,3 +21,6 @@ navToggle.addEventListener('click', () => {
     mainNav.classList.toggle('active'); // Toggle main-nav
     navToggle.setAttribute('aria-expanded', mainNav.classList.contains('active'));//toggle aria-expanded
 });
+
+//Get year for copyright footprint
+document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
